55e98e42c9f8371260f67b258a450ab8cdc9c57e
Sean Knox <knoxville@gmail.com>
2018-06-28 17:50:55 -0700

    (fix): remove waagent install, as official ubuntu image already has it

--- packer/install-core-packages.sh
+++ packer/install-core-packages.sh
@@ -8,4 +8,4 @@ apt_flags=(-o "Dpkg::Options::=--force-confnew" -qy)
 apt-get update -q
 apt-get upgrade "${apt_flags[@]}"
 
-apt-get install "${apt_flags[@]}" conntrack ipset socat jq traceroute ca-certificates waagent
+apt-get install "${apt_flags[@]}" conntrack ipset socat jq traceroute ca-certificates
